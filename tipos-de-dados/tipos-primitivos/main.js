d58f5eba1c4a652b14b3f00961a6dd32e9faeb4d
Gustavo Alves <gustavo_alves777@outlook.com>
2021-09-06 16:38:32 -0300

    aula de tipos de dados finalizada

---
+++ tipos-de-dados/tipos-primitivos/main.js
@@ -0,0 +1,8 @@
+const nome = "Gustavo"; // string
+const nome1 = "Rodolfo"; // string
+const nome2 = "Alberto"; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined => não aponta nengum local da memoria
+const sobrenomeAluno = null; // Nulo => não aponta nenhum local na memoria
+const boolean = true || false; // boolean (valor lógico)
---
+++ variaveis/var-e-let/main.js
@@ -0,0 +1,3 @@
+let nome = 'Gustavo Alves Silva';
+
+// nao podemos redeclar variavel let so podemos redeclarar com var
