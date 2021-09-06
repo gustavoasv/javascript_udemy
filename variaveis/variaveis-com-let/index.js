588fcb578c024d7c51227ad6e60e9571df0db7a7
Gustavo Alves <gustavo_alves777@outlook.com>
2021-09-06 14:05:11 -0300

    initial commit

---
+++ variaveis/variaveis-com-let/index.js
@@ -0,0 +1,10 @@
+// Variaveis servem pra guardar dados na memoria do computador
+
+//Regras:
+
+
+// a variavel let pode ter seu valor alterado
+// Variaveis precisam de nomes significativos
+// Não pode começar o nome da varivael com numero
+// Utilizar camelCase
+// Variaveis são Case-sensitive letra maiuscula e minuscula faz diferença
