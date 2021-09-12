27cf39a86befafcb0b7d5e557434a76920f7c9dd
Gustavo Alves <gustavo_alves777@outlook.com>
2021-09-12 14:48:23 -0300

    aula sobre metodos Math

--- exercicio-com-variaveis/main.js
+++ exercicio-com-variaveis/main.js
@@ -1,15 +1,28 @@
 
-function ver(){
-    const nome = document.querySelector('input').value
+// function ver(){
+//     const nome = document.querySelector('input').value
+//     let el = document.querySelector(".ex")
+
+//      el.innerHTML = `<p>Seu nome é ${nome}</p>`
+//      el.innerHTML += `<p>Seu nome tem ${nome.length} letras</p>`
+//      el.innerHTML += `<p>A segunda letra do seu nome é ${nome[1]}</p>`
+
+//      el.innerHTML += `<p>As ultimas 3 letras do seu nome são ${nome.slice(-3)}</p>`
+//      el.innerHTML += `<p>As palavras do seu nome são ${nome.split(', ')}</p>`;
+//      el.innerHTML += `<p>Seu nome com letras maiuscula ${nome.toUpperCase()}</p>`;
+//      el.innerHTML += `<p>Seu nome com letras mainusculas ${nome.toLocaleLowerCase()}</p>`;
+   
+// }
+
+
+
+ function ver(){
+     const nome = document.querySelector('input').value
     let el = document.querySelector(".ex")
 
-     el.innerHTML = `<p>Seu nome é ${nome}</p>`
-     el.innerHTML += `<p>Seu nome tem ${nome.length} letras</p>`
-     el.innerHTML += `<p>A segunda letra do seu nome é ${nome[1]}</p>`
+    el.innerHTML = `<h2>${nome} é gente boa</h2>`;
 
-     el.innerHTML += `<p>As ultimas 3 letras do seu nome são ${nome.slice(-3)}</p>`
-     el.innerHTML += `<p>As palavras do seu nome são ${nome.split(', ')}</p>`;
-     el.innerHTML += `<p>Seu nome com letras maiuscula ${nome.toUpperCase()}</p>`;
-     el.innerHTML += `<p>Seu nome com letras mainusculas ${nome.toLocaleLowerCase()}</p>`;
-   
-}
+    if(nome == 'Juliano'){
+        el.innerHTML = `<h2>${nome} é viado</h2>`;
+    }
+ }
---
+++ objeto-math/main.js
@@ -0,0 +1,19 @@
+let num1 = 9.54578;
+let arr = Math.floor(num1);
+let arr2 = Math.ceil(num1);
+console.log(arr, arr2);
+
+// Math.floor == arredonda pra baixo
+// Math.ceil == arredonda pra cima
+
+let num2 = Math.round(2.5);
+
+console.log(num2);
+console.log(Math.max(1, 2, 3, 4, 90, 39, 38493, 3444, 463636, 3463643, 332535))
+
+console.log(Math.random() * (10 - 5) + 5) // retorna um numero aleatorio entre 10 e 5
+
+// Math.round == arredonda pra um numero mais proximo
+// Math.max == captura o maior numero
+// Math.min == captura o menor numero
+// Math.random == retorna um numero aleatorio
